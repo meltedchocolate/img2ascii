31b6f226bfac9eda8726a90dd6f77859999cce83
Patrick LeDew <meltedchocolate@users.noreply.github.com>
2022-02-07 09:44:08 -0500

    Comments

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const rampLength = grayRamp.length - 1;
 const img2ascii = (path, width, invert = false) => {
     let height = width; //Assume a 1 to 1 aspect ratio initially
     let str = '';
-    let image = sharp(path);
+    let image = sharp(path); // Get image from path
     image.metadata().then(data => {
-        let aspectRatio = data.width / data.height;
-        height = Math.floor((width / aspectRatio) / 2);
+        height = Math.floor(data.height / 2); // Since ASCII characters are taller than they are wide, set height to be half that of
     }).then(() => {
         image.sharpen(100).removeAlpha().resize(width, height, { fit: "fill" }).raw().toBuffer().then(val => {
             for (let px = 0; px < width * height; px++) {
-                if (px % width == 0) str += '\n';
+                if (px % width == 0) str += '\n'; // Add line break at end of row
                 let r = val[px * 3 + 0];
                 let g = val[px * 3 + 1];
                 let b = val[px * 3 + 2];
-                let bwVal = ((r + g + b) / 3.0) / 255.0;
+                let bwVal = ((r + g + b) / 3.0) / 255.0; // Get grayscale values
                 let asciiVal = Math.floor((invert ? bwVal : (bwVal * -1.0 + 1.0)) * rampLength); //Reverse the values (so darker is a higher value)
                 str += grayRamp[asciiVal];
             }
             console.log(str);
+            // Post final text to hastebin
             fetch("https://hastebin.com/documents", {
                 method: "POST",
                 body: str,
@@ -30,7 +30,11 @@ const img2ascii = (path, width, invert = false) => {
                     "Content-Type": "text/plain"
                 }
             }).then(response => {
-                response.json().then(json => { let link = "https://hastebin.com/raw/" + json["key"]; console.log(link); return link; }).catch(err => { console.log(err) });
+                response.json().then(json => {
+                    let link = "https://hastebin.com/raw/" + json["key"];
+                    console.log(link); // Print this link
+                    return link; // Return the link
+                }).catch(err => { console.log(err) });
             }).catch((err) => { console.log(err); throw err; });
         }).catch(err => { console.log(err); throw err; });
     }).catch(err => { console.log(err); throw err; });
